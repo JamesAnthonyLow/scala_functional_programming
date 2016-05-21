651678da049276d0c4eeb19875df461e9050733c
Ronald Schmitt <Ron.Schmitt@caa.columbia.edu>
2016-05-21 15:33:57 -0400

    Update ron - Area Under Curves and Volume of Revolving a Curve.scala

--- ron - Area Under Curves and Volume of Revolving a Curve.scala
+++ ron - Area Under Curves and Volume of Revolving a Curve.scala
@@ -1,32 +1,26 @@
-// This function will be used while invoking "Summation" to compute
- // The area under the curve.
- def f(coefficients:List[Int],powers:List[Int],x:Double):Double =
-   {
-      //Fill Up this function body
-        // To compute the value of the function
-        // For the given coefficients, powers and value of x
-   }
 
-  // This function will be used while invoking "Summation" to compute 
-  // The Volume of revolution of the curve around the X-Axis
-  // The 'Area' referred to here is the area of the circle obtained
-  // By rotating the point on the curve (x,f(x)) around the X-Axis
-  def area(coefficients:List[Int],powers:List[Int],x:Double):Double = 
-   {
-      //Fill Up this function body
-        // To compute the area of the circle on revolving the point
-         // (x,f(x)) around the X-Axis
-        // For the given coefficients, powers and value of x
-   }
+import scala.math._
 
-   // This is the part where the series is summed up
-   // This function is invoked once with func = f to compute the area         // under the curve
-    // Then it is invoked again with func = area to compute the volume 
-    // of revolution of the curve
-   def summation(func:(List[Int],List[Int],Double)=>Double,upperLimit:Int,lowerLimit:Int,coefficients:List[Int],powers:List[Int]):Double =
-   {
-      // Fill up this function
-   }
+def f(coefficients:List[Int], powers:List[Int], x:Double):Double = {
+  require(coefficients.size == powers.size)
+  return List.tabulate(powers.size)( n => coefficients.apply(n) * pow(x,powers.apply(n))   ).sum;
+}
+
+// The 'Area' referred to here is the area of the circle obtained
+// By rotating the point on the curve (x,f(x),0) around the X-Axis
+def area(coefficients:List[Int], powers:List[Int], x:Double):Double = {
+  require(coefficients.size == powers.size)
+  val y = f(coefficients, powers, x);
+  return Pi * pow(y, 2);
+}
 
 
-// The Input-Output functions will be handled by us. You only need to concentrate your effort on the function bodies above.
+// integral
+def summation(func:(List[Int],List[Int],Double)=>Double, b:Int, a:Int, coefficients:List[Int], powers:List[Int] ):Double = {
+val dx0 : Double = 0.001;
+val N : Int = ((b - a).toDouble/dx0).round.toInt;
+val ns = 0 to N;
+val dx : Double = (b - a).toDouble/N.toDouble;
+ns.map( n =>  func(coefficients, powers, a.toDouble+n*dx) * dx  ).sum;
+}
+
